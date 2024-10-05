c4d186c6b798f45c0c7cb7688d1b96e2e122f73c
MetalGear-ia <andregranados840@gmail.com>
2024-10-05 08:09:24 -0500

    Add files via upload

---
+++ contar.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class contar {
+/** contar-vocales */
+   public static void main(String[] args) {
+    // Variables
+    Scanner scanner = new Scanner(System.in);
+    String texto;
+    char[] caracteres;
+
+    // Entrada
+    System.out.println("Ingrese texto para identificar las vocales");
+    texto = scanner.nextLine();
+    texto = texto.toLowerCase();
+
+
+   }
+}
